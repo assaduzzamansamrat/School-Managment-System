3e0b464c4ea195aff6151690d95af3e62d17bc04
assaduzzamansamrat <assaduzzamansamrat@gmail.com>
2018-09-26 13:38:27 +0600

    first commit

---
+++ SchoolManagementSolution/SchoolManagementRepository/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementRepository
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DOB { get; set; }
+        public string ClassNumber { get; set; }
+        public int Roll { get; set; }
+        public string Gender { get; set; }
+        public string Religion { get; set; }
+        public string Email { get; set; }
+        public string FatherName { get; set; }
+        public string FatherOccupation { get; set; }
+        public string MotherName { get; set; }
+        public string MotherOccupation { get; set; }
+        public string GurdianPhoneNo { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public string AdmissionDate { get; set; }
+
+    }
+}
